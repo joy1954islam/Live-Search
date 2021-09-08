95b39084633c965a909fff6caf18ce5e353f728b
joy1954islam <joyislam1954@gmail.com>
2021-09-08 22:43:25 +0600

    backend and frontend added EmployeeListView

--- search_1/views.py
+++ search_1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Employee
+import json
 
-# Create your views here.
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = 'main.html'
+    context_object_name = 'employees'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["qs_json"] = json.dumps(list(Employee.objects.values()))
+        return context
