4ad0e590bd1adc11342ddf5b1cd1301505490dee
joy1954islam <joyislam1954@gmail.com>
2021-09-08 22:14:07 +0600

    create a models name employee and migrate

--- search_1/models.py
+++ search_1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField('Employee First Name', max_length=25)
+    last_name = models.CharField('Employee Last Name', max_length=25)
+    email = models.EmailField('Email')
+    address = models.TextField('Address', null=True, blank=True)
+    age = models.IntegerField('Age')
+    city = models.CharField(max_length=35)
+
+    def __str__(self):
+        return str(self.email)
